0fc5a1c47eff384588d6807672ff97f496a86386
Lenny Brown <penn2014@gmail.com>
2020-07-06 19:07:47 -0700

    Week 2 nearly completed

---
+++ Week 2 - Express/index.js
@@ -0,0 +1,43 @@
+'use strict';
+const express = require("express");
+const bodyParser = require("body-parser");
+let exphbs = require("express-handlebars");
+
+const team = require('./models/data.js');
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname +'/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('handlebars', exphbs({defaultLayout: false}));
+app.set("view engine", "handlebars");
+
+app.get('/', (req, res) => {
+    res.render('home', { team: team.getAll() });
+});
+
+app.get('/detail/:id', (req, res) => {
+    res.render('detail', { item: team.getTeamMember(req.params.id) });
+});
+
+app.get('/about', (req, res) => {
+    res.type('text/html');
+    res.status(200);
+    const firstLine = "Website for all kinds of people, made by Lenny Brown. ";
+    const secondLine = "My life path so far is Philadelphia -> Rochester -> Thailand -> Seattle. ";
+    const thirdLine =  "I was in the Data Science program at University of Rochester, but now ";
+    const fourthLine = "I'm in the App Dev program at North Seattle College.";
+
+    res.end(`<p style="padding: 50px;">${firstLine}${secondLine}${thirdLine}${fourthLine}</p>`);
+});
+
+app.use( (req, res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), () => {
+    console.log("Express Started");
+});
---
+++ Week 2 - Express/models/data.js
@@ -0,0 +1,149 @@
+let team = [{
+    "id": 1,
+    "firstName": "Yetty",
+    "lastName": "Corbie",
+    "favoriteWord": "superstructure",
+    "favoritePhrase": "Inverse cohesive function",
+    "skill": "SRM 5.0"
+  }, {
+    "id": 2,
+    "firstName": "Wally",
+    "lastName": "Clawe",
+    "favoriteWord": "Implemented",
+    "favoritePhrase": "Persevering zero defect help-desk",
+    "skill": "qPCR"
+  }, {
+    "id": 3,
+    "firstName": "Guilbert",
+    "lastName": "Akenhead",
+    "favoriteWord": "Progressive",
+    "favoritePhrase": "Implemented logistical access",
+    "skill": "LCD TV"
+  }, {
+    "id": 4,
+    "firstName": "Cy",
+    "lastName": "Cazin",
+    "favoriteWord": "success",
+    "favoritePhrase": "Virtual intermediate secured line",
+    "skill": "Zumba Instruction"
+  }, {
+    "id": 5,
+    "firstName": "Juan",
+    "lastName": "Canero",
+    "favoriteWord": "structure",
+    "favoritePhrase": "Robust optimal throughput",
+    "skill": "HR Policy Formulation"
+  }, {
+    "id": 6,
+    "firstName": "Fidel",
+    "lastName": "Buche",
+    "favoriteWord": "Multi-channelled",
+    "favoritePhrase": "Open-source asymmetric circuit",
+    "skill": "SFE"
+  }, {
+    "id": 7,
+    "firstName": "Rurik",
+    "lastName": "Raybould",
+    "favoriteWord": "open system",
+    "favoritePhrase": "Reverse-engineered multimedia Graphical User Interface",
+    "skill": "Rheumatoid Arthritis"
+  }, {
+    "id": 8,
+    "firstName": "Stanwood",
+    "lastName": "Lago",
+    "favoriteWord": "definition",
+    "favoritePhrase": "Persistent 4th generation website",
+    "skill": "XML Standards"
+  }, {
+    "id": 9,
+    "firstName": "Pennie",
+    "lastName": "Brende",
+    "favoriteWord": "intangible",
+    "favoritePhrase": "Streamlined 6th generation knowledge base",
+    "skill": "Hydraulic Systems"
+  }, {
+    "id": 10,
+    "firstName": "Corinne",
+    "lastName": "Gauchier",
+    "favoriteWord": "pricing structure",
+    "favoritePhrase": "Balanced mission-critical firmware",
+    "skill": "HDX"
+  }, {
+    "id": 11,
+    "firstName": "Terrance",
+    "lastName": "Reston",
+    "favoriteWord": "Focused",
+    "favoritePhrase": "Streamlined bi-directional data-warehouse",
+    "skill": "OWA"
+  }, {
+    "id": 12,
+    "firstName": "Rafi",
+    "lastName": "Dallyn",
+    "favoriteWord": "functionalities",
+    "favoritePhrase": "Focused optimizing challenge",
+    "skill": "WAN"
+  }, {
+    "id": 13,
+    "firstName": "Kaitlyn",
+    "lastName": "Ouver",
+    "favoriteWord": "zero tolerance",
+    "favoritePhrase": "Integrated even-keeled emulation",
+    "skill": "Shell Scripting"
+  }, {
+    "id": 14,
+    "firstName": "Britney",
+    "lastName": "Wyvill",
+    "favoriteWord": "executive",
+    "favoritePhrase": "Sharable tertiary strategy",
+    "skill": "TMN"
+  }, {
+    "id": 15,
+    "firstName": "Demetria",
+    "lastName": "Wilmut",
+    "favoriteWord": "Customizable",
+    "favoritePhrase": "Universal intermediate adapter",
+    "skill": "CNC Mill"
+  }, {
+    "id": 16,
+    "firstName": "Dory",
+    "lastName": "Doe",
+    "favoriteWord": "Synergized",
+    "favoritePhrase": "Robust even-keeled model",
+    "skill": "EBA"
+  }, {
+    "id": 17,
+    "firstName": "Zacharias",
+    "lastName": "Rummery",
+    "favoriteWord": "function",
+    "favoritePhrase": "Customizable executive workforce",
+    "skill": "Trade Shows"
+  }, {
+    "id": 18,
+    "firstName": "Henka",
+    "lastName": "Devenport",
+    "favoriteWord": "scalable",
+    "favoritePhrase": "Decentralized empowering task-force",
+    "skill": "XStream"
+  }, {
+    "id": 19,
+    "firstName": "Ortensia",
+    "lastName": "Grimes",
+    "favoriteWord": "bifurcated",
+    "favoritePhrase": "Versatile homogeneous open system",
+    "skill": "WCF"
+  }, {
+    "id": 20,
+    "firstName": "Mikaela",
+    "lastName": "Ubanks",
+    "favoriteWord": "Public-key",
+    "favoritePhrase": "Persistent radical pricing structure",
+    "skill": "Requirements Gathering"
+}];
+
+exports.getAll = () => {
+  return team;
+}
+
+exports.getTeamMember = (memberId) => {
+  return team[memberId - 1];
+}
